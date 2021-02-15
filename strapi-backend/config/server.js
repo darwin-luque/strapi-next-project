6d09f0da6f33d2c7b84ac74bd3f7c835629905f3
iamdperez <danperez27@gmail.com>
2021-02-15 10:12:55 -0600

    Using strapi docker image

--- strapi-backend/config/server.js
+++ strapi-backend/config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a7c5e60027b0e4ffe5cdd1b31f5b63b2'),
+      secret: env('ADMIN_JWT_SECRET', 'f9add01977f7d731a39a4ab16d24aae4'),
     },
   },
 });
--- strapi-backend/extensions/users-permissions/config/jwt.js
+++ strapi-backend/extensions/users-permissions/config/jwt.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'ee08b172-93d5-4593-a4a5-abdba8c8da07'
+  jwtSecret: process.env.JWT_SECRET || '4c5af61f-2e37-4e2c-9a11-95cb07835268'
 };
